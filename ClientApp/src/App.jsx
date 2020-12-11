766ba5c384576c1e04d0f8e63f30273187f62332
TmasLee <thomas@livebuildingsystems.com>
2020-12-11 14:47:20 -0500

    Set up webpack and react

---
+++ ClientApp/src/App.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import {hot} from "react-hot-loader";
+
+
+class App extends Component {
+    render(){
+        return(
+            <div></div>
+        );
+    }
+}
+
+export default hot(module)(App);
