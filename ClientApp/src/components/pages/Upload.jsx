d568408229b2342499f78bd415ac0165b1ea4816
TmasLee <thomas@livebuildingsystems.com>
2020-12-11 18:00:25 -0500

    Set up scripts and page components

---
+++ ClientApp/src/components/pages/Upload.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Upload extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Upload</h1>
+            </div>
+        );
+    }
+}
