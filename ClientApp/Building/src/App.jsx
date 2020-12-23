73dd4fe5cdc398449b4c848bde1361d94a0b330b
TmasLee <thomas@livebuildingsystems.com>
2020-12-22 22:17:51 -0500

    Building Spa inital files

---
+++ ClientApp/Building/src/App.jsx
@@ -0,0 +1,16 @@
+
+import React, { Component } from "react";
+import { Route } from "react-router";
+import { hot } from "react-hot-loader";
+
+import "../../css/BuildingApp.css";
+
+class App extends Component {
+    render(){
+        return(
+            <div>building app</div>
+        );
+    }
+}
+
+export default hot(module)(App);
