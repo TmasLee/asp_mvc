deb983e449e31cba7ff1f4332bdf3eb301670bce
TmasLee <thomas@livebuildingsystems.com>
2020-12-31 19:51:36 -0500

    Update User to have Id

---
+++ Migrations/20210101004837_AddIdToUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace asp_mvc.Migrations
+{
+    public partial class AddIdToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "User");
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -5,6 +5,7 @@ namespace asp_mvc.Models
 {
     public class User
     {
+        public int Id { get; set; }
         [Key]
         public string Email { get; set; }
         public string FirstName { get; set; }
