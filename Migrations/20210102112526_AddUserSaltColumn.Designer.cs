258b618ff420041b60f159ea93a7ecdf123aa23b
TmasLee <thomas@livebuildingsystems.com>
2021-01-02 06:26:47 -0500

    Add salt column

--- Migrations/20210102112526_AddUserSaltColumn.Designer.cs
+++ Migrations/20210102112526_AddUserSaltColumn.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using asp_mvc.Data;
 
 namespace asp_mvc.Migrations
 {
     [DbContext(typeof(MSAContext))]
-    partial class MSAContextModelSnapshot : ModelSnapshot
+    [Migration("20210102112526_AddUserSaltColumn")]
+    partial class AddUserSaltColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/User.cs
+++ Models/User.cs
@@ -11,6 +11,7 @@ namespace asp_mvc.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Password { get; set; }
+        public string Salt { get; set; }
         public ICollection<Building> Buildings { get; set; }
     }
 }
