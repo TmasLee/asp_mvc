3ad1919f3b3d27780831768f6b3450e1a36196ee
TmasLee <thomas@livebuildingsystems.com>
2021-01-30 04:09:38 -0500

    Disable modal toggle during load

--- ClientApp/Client/src/components/generics/FormModal.jsx
+++ ClientApp/Client/src/components/generics/FormModal.jsx
@@ -6,7 +6,7 @@ export class FormModal extends Component {
     render(){
         const { showModal, title, primaryButtonMsg, resetAndToggleModal, action, loading } = this.props;
         return (
-            <Modal show={showModal} onHide={resetAndToggleModal}>
+            <Modal show={showModal} onHide={resetAndToggleModal} backdrop={loading ? 'static' : true}>
                 <Modal.Header>{title}</Modal.Header>
                 <Modal.Body>
                     {this.props.children}
@@ -15,10 +15,12 @@ export class FormModal extends Component {
                     {
                         loading ? <LoadingButton/>
                         :
-                        <Button variant="primary" onClick={action}>{primaryButtonMsg}</Button>
+                        <div>
+                            <Button variant="primary" onClick={action}>{primaryButtonMsg}</Button>
+                            {' '}
+                            <Button variant="secondary" onClick={resetAndToggleModal}>Cancel</Button>
+                        </div>
                     }
-                    {' '}
-                    <Button variant="secondary" onClick={resetAndToggleModal}>Cancel</Button>
                 </Modal.Footer>
             </Modal>
         )
